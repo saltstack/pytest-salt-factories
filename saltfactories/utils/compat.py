75be2a10a10e56623bc6ad170aad40de9b08bd4d
Pedro Algarvio <pedro@algarvio.me>
2019-09-09 17:27:48 +0100

    Add mock to compat

--- saltfactories/utils/compat.py
+++ saltfactories/utils/compat.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 '''
-pytestsalt.utils.compat
-~~~~~~~~~~~~~~~~~~~~~~~
+saltfactories.utils.compat
+~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Imports compatability layer
 '''
-# pylint: disable=unused-import
+# pylint: disable=unused-import,invalid-name
 
 try:
     # Salt > 2017.1.1
-    # pylint: disable=invalid-name
     import salt.utils.files
 
     fopen = salt.utils.files.fopen
 except AttributeError:
     # Salt <= 2017.1.1
-    # pylint: disable=invalid-name
     fopen = salt.utils.fopen
 
 try:
     # Salt >= 2018.3.0
-    # pylint: disable=invalid-name
     from salt.utils.path import which
 except ImportError:
     # Salt < 2018.3.0
-    # pylint: disable=invalid-name
     from salt.utils import which
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+# pylint: enable=unused-import,invalid-name
