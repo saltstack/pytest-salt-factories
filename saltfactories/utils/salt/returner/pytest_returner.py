19336720f075e7d8f067f71de8b59e7e933937e4
Pedro Algarvio <pedro@algarvio.me>
2020-02-20 10:17:46 +0000

    ZMQ Connection should now be pickle'able

--- saltfactories/utils/salt/returner/pytest_returner.py
+++ saltfactories/utils/salt/returner/pytest_returner.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import atexit
 import logging
 
 import salt.utils.msgpack as msgpack
@@ -18,21 +19,56 @@ except ImportError:
 log = logging.getLogger(__name__)
 
 
-def _get_conn(opts, ctx):
-    if "zmq_context" not in ctx:
-        context = zmq.Context()
-        ctx["zmq_context"] = context
-    else:
-        context = ctx["zmq_context"]
+class Connection(object):
+    def __init__(self, address):
+        self.address = address
+        self.context = zmq.Context()
+        self.push = self.context.socket(zmq.PUSH)
+        log.debug("Connecting PUSH socket to %s", self.address)
+        self.push.connect(self.address)
+        atexit.register(self.close)
 
-    if "zmq_socket" not in ctx:
-        push = context.socket(zmq.PUSH)
-        log.debug("Connecting PUSH socket to %s", opts["pytest_returner_address"])
-        push.connect(opts["pytest_returner_address"])
-        ctx["zmq_socket"] = push
+    def __getstate__(self):
+        return {"address": self.address}
+
+    def __setstate__(self, state):
+        return self.__init__(state["address"])
+
+    def close(self):
+        if self.push:
+            self.push.close(1)
+            self.push = None
+        if self.context:
+            self.context.term()
+            self.context = None
+
+    def send(self, data):
+        self.push.send(data)
+
+
+__virtualname__ = "pytest_returner"
+
+
+def __virtual__():
+    if msgpack.HAS_MSGPACK is False:
+        msg = "The 'msgpack' python library is not available."
+        log.warning(msg)
+        return False, msg
+    if HAS_ZMQ is False:
+        msg = "The 'pyzmq' python library is not available."
+        log.warning(msg)
+        return False, msg
+    log.warning("%s should be good to load", __virtualname__)
+    return True
+
+
+def _get_conn(opts, ctx):
+    if "zmq_conn" not in ctx:
+        conn = Connection(opts["pytest_returner_address"])
+        ctx["conn"] = conn
     else:
-        push = ctx["zmq_socket"]
-    return push
+        conn = ctx["zmq_conn"]
+    return conn
 
 
 def event_return(events):
