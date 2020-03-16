11c8850a362bdf4730e083c55a34e464a82b2fce
Pedro Algarvio <pedro@algarvio.me>
2020-03-16 14:04:56 +0000

    Add support to start a SSHD daemon, including tests.

---
+++ saltfactories/utils/processes/sshd.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+    saltfactories.utils.processes.sshd
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    SSHD daemon process implementation
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import textwrap
+
+from saltfactories.exceptions import ProcessFailed
+from saltfactories.utils import ports
+from saltfactories.utils.processes.bases import FactoryDaemonScriptBase
+from saltfactories.utils.processes.bases import Popen
+
+
+class SshdDaemon(FactoryDaemonScriptBase):
+    def __init__(self, *args, **kwargs):
+        config_dir = kwargs.pop("config_dir")
+        serve_port = kwargs.pop("serve_port", None)
+        super(SshdDaemon, self).__init__(*args, **kwargs)
+        self.config_dir = config_dir
+        self.serve_port = serve_port or ports.get_unused_localhost_port()
+        sshd_config_file = os.path.join(config_dir, "sshd_config")
+        if not os.path.isfile(sshd_config_file):
+            # Let's generat the host keys
+            host_keys = []
+            for key_type in ("dsa", "rsa"):
+                key_path = os.path.join(config_dir, "ssh_host_{}_key".format(key_type))
+                if not os.path.exists(key_path):
+                    cmdline = ["ssh-keygen", "-f", key_path, "-N", "", "-t", key_type]
+                    proc = Popen(cmdline)
+                    stdout, stderr = proc.communicate()
+                    if proc.returncode:
+                        raise ProcessFailed(
+                            "Failed to generate {} key.",
+                            cmdline=cmdline,
+                            stdout=stdout,
+                            stderr=stderr,
+                        )
+                os.chmod(key_path, 0o0400)
+                host_keys.append(key_path)
+
+            # Let's write a default config file
+            with open(sshd_config_file, "w") as wfh:
+                wfh.write(
+                    textwrap.dedent(
+                        """\
+                Port {}
+                ListenAddress 127.0.0.1
+                PermitRootLogin no
+                ChallengeResponseAuthentication no
+                PasswordAuthentication no
+                PubkeyAuthentication yes
+                PrintMotd no
+                """.format(
+                            self.serve_port,
+                        )
+                    )
+                )
+                for host_key in host_keys:
+                    wfh.write("HostKey {}\n".format(host_key))
+            os.chmod(sshd_config_file, 0o0600)
+
+    def get_base_script_args(self):
+        """
+        Returns any additional arguments to pass to the CLI script
+        """
+        return ["-D", "-e", "-f", os.path.join(self.config_dir, "sshd_config")]
+
+    def get_serve_ports(self):
+        """
+        Return a list of ports to check against to ensure the daemon is running
+        """
+        return [self.serve_port]
---
+++ tests/integration/factories/ssh/test_sshd.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import shutil
+
+import pytest
+
+from saltfactories.utils.processes.sshd import SshdDaemon
+
+
+@pytest.fixture(scope="module")
+def sshd_config_dir(salt_factories):
+    config_dir = salt_factories.root_dir.join("sshd").ensure(dir=True)
+    yield config_dir.strpath
+    shutil.rmtree(config_dir.strpath, ignore_errors=True)
+
+
+@pytest.fixture(scope="module")
+def sshd(request, sshd_config_dir, salt_factories):
+    return salt_factories.spawn_daemon(
+        request, "sshd", SshdDaemon, "test-sshd", config_dir=sshd_config_dir, cwd=sshd_config_dir
+    )
+
+
+def test_sshd(sshd):
+    assert sshd.is_alive()
