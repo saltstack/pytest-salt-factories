c0ad0d2761cc4d630996d9e3e791b71a0df998eb
Pedro Algarvio <pedro@algarvio.me>
2020-03-16 10:46:28 +0000

    Add initial process statistics tests

---
+++ tests/functional/test_sys_stats.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.functional.test_sys_stats
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests related to processes system statistics enabled by the `--sys-stats` flag.
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def test_basic_sys_stats(testdir):
+    p = testdir.makepyfile(
+        """
+        def test_one():
+            assert True
+        """
+    )
+    res = testdir.runpytest("-vv", "--sys-stats")
+    res.assert_outcomes(passed=1)
+    res.stdout.fnmatch_lines(
+        [
+            "* PASSED*",
+            "* Processes Statistics *",
+            "* System  -  CPU:  * %   MEM:  * % (Virtual Memory)",
+            "* Test Suite Run  -  CPU:   * %   MEM:   * % (RSS)",
+            "* 1 passed in *",
+        ]
+    )
+
+
+def test_basic_sys_stats_uss(testdir):
+    p = testdir.makepyfile(
+        """
+        def test_one():
+            assert True
+        """
+    )
+    res = testdir.runpytest("-vv", "--sys-stats", "--sys-stats-uss-mem")
+    res.assert_outcomes(passed=1)
+    res.stdout.fnmatch_lines(
+        [
+            "* PASSED*",
+            "* Processes Statistics *",
+            "* System  -  CPU:  * %   MEM:  * % (Virtual Memory)",
+            "* Test Suite Run  -  CPU:   * %   MEM:   * % (USS)",
+            "* 1 passed in *",
+        ]
+    )
