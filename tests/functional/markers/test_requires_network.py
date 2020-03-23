f26af1c8269193083870d33ffea46796f76d2deb
Pedro Algarvio <pedro@algarvio.me>
2020-03-23 12:42:01 +0000

    Add functional tests for `@pytest.mark.requires_network`

---
+++ tests/functional/markers/test_requires_network.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.functional.markers.test_requires_network
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Test the ``@pytest.mark.requires_network`` marker
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import socket
+
+import mock
+import pytest
+
+from saltfactories.utils import ports
+
+
+def test_has_local_network(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.requires_network
+        def test_one():
+            assert True
+        """
+    )
+    res = testdir.runpytest()
+    res.assert_outcomes(passed=1)
+    try:
+        res.stdout.no_fnmatch_line("*PytestUnknownMarkWarning*")
+    except AttributeError:
+        # PyTest 4.6.x
+        from _pytest.outcomes import Failed
+
+        with pytest.raises(Failed):
+            res.stdout.fnmatch_lines(
+                ["*PytestUnknownMarkWarning*",]
+            )
+
+
+def test_no_local_network(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.requires_network
+        def test_one():
+            assert True
+        """
+    )
+    mock_socket = mock.MagicMock()
+    mock_socket.bind = mock.MagicMock(side_effect=socket.error)
+    with mock.patch(
+        "saltfactories.utils.ports.get_unused_localhost_port",
+        side_effect=[ports.get_unused_localhost_port(), ports.get_unused_localhost_port()],
+    ):
+        with mock.patch("socket.socket", return_value=mock_socket):
+            res = testdir.runpytest_inprocess()
+            res.assert_outcomes(skipped=1)
+    try:
+        res.stdout.no_fnmatch_line("*PytestUnknownMarkWarning*")
+    except AttributeError:
+        # PyTest 4.6.x
+        from _pytest.outcomes import Failed
+
+        with pytest.raises(Failed):
+            res.stdout.fnmatch_lines(
+                ["*PytestUnknownMarkWarning*",]
+            )
