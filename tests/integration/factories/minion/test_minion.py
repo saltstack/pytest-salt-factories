f70c206881d18096d20b69f61f7c24df3b4eb074
Pedro Algarvio <pedro@algarvio.me>
2020-03-30 14:17:35 +0100

    Copy a failing test from salt's test suite

--- tests/integration/factories/minion/test_minion.py
+++ tests/integration/factories/minion/test_minion.py
@@ -26,6 +26,11 @@ def salt_call_cli(salt_factories, minion, master):
     return salt_factories.get_salt_call_cli(minion.config["id"])
 
 
+@pytest.fixture
+def salt_run_cli(salt_factories, minion, master):
+    return salt_factories.get_salt_run_cli(master.config["id"])
+
+
 def test_minion(minion, salt_cli):
     assert minion.is_alive()
     ret = salt_cli.run("test.ping", minion_tgt="minion-1")
@@ -63,3 +68,27 @@ def test_salt_call_exception_handling_doesnt_timeout(minion, salt_call_cli):
         "test.raise_exception", "OSError", "2", "No such file or directory", "/tmp/foo.txt"
     )
     assert ret.exitcode == 1, ret
+
+
+@pytest.mark.skip_on_windows
+def test_return(salt_call_cli, salt_run_cli):
+    """
+    This is a failing test on Salt's test suite
+    """
+    command = "echo returnTOmaster"
+    ret = salt_call_cli.run("cmd.run", command)
+    assert ret.exitcode == 0
+    assert ret.json == "returnTOmaster"
+
+    ret = salt_run_cli.run("jobs.list_jobs")
+    assert ret.exitcode == 0
+    jid = target = None
+    for jid, details in ret.json.items():
+        if command in details["Arguments"]:
+            target = details["Target"]
+            break
+
+    ret = salt_run_cli.run("jobs.lookup_jid", jid, _timeout=60)
+    assert ret.exitcode == 0
+    assert target in ret.json
+    assert ret.json[target] == "returnTOmaster"
