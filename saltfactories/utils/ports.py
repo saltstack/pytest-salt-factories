5600d1f56447ea548f0a43fa2146abf23f227a5d
Pedro Algarvio <pedro@algarvio.me>
2020-04-16 15:14:20 +0100

    Allow providing the ammount of seconds to cache a port

--- saltfactories/utils/ports.py
+++ saltfactories/utils/ports.py
@@ -13,16 +13,28 @@ import time
 log = logging.getLogger(__name__)
 
 
-def get_unused_localhost_port():
+def get_unused_localhost_port(cached_seconds=1):
     """
     Return a random unused port on localhost
     """
+    if not isinstance(cached_seconds, (int, float)):
+        raise RuntimeError(
+            "The value of 'cached_seconds' needs to be an integer or a float, not {}".format(
+                type(cached_seconds)
+            )
+        )
+    if cached_seconds < 0:
+        raise RuntimeError(
+            "The value of 'cached_seconds' needs to be a positive number, not {}".format(
+                cached_seconds
+            )
+        )
     try:
         generated_ports = get_unused_localhost_port.__used_ports__
         # Cleanup ports. The idea behind this call is so that two consecutive calls to this
         # function don't return the same port just because the first call hasn't actuallt started
         # using the port.
-        # It also makes this cache invalid after 1 second
+        # It also makes this cache invalid after <cached_seconds> second
         for port in list(generated_ports):
             if generated_ports[port] <= time.time():
                 generated_ports.pop(port)
@@ -33,7 +45,7 @@ def get_unused_localhost_port():
         usock.bind(("127.0.0.1", 0))
         port = usock.getsockname()[1]
     if port not in generated_ports:
-        generated_ports[port] = time.time() + 1
+        generated_ports[port] = time.time() + cached_seconds
         return port
     return get_unused_localhost_port()
 
