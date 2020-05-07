f6511ede6b8a0e756b9b696397b547180d11ae55
Pedro Algarvio <pedro@algarvio.me>
2020-05-07 19:00:29 +0100

    Fix duplicate keywords

--- tests/functional/factories/conftest.py
+++ tests/functional/factories/conftest.py
@@ -11,20 +11,12 @@ from saltfactories.factories import manager
 
 @pytest.fixture
 def salt_factories(
-    request,
-    pytestconfig,
-    tempdir,
-    log_server,
-    log_server_port,
-    log_server_level,
-    salt_factories_config,
+    request, pytestconfig, tempdir, log_server, salt_factories_config,
 ):
     with mock.patch("saltfactories.utils.event_listener.EventListener"):
         return manager.SaltFactoriesManager(
             pytestconfig,
             tempdir,
-            log_server_port,
-            log_server_level,
             stats_processes=request.session.stats_processes,
             **salt_factories_config
         )
