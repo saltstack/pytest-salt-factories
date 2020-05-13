286b2e76a0b704e91edf9fb432697122b62d3812
Pedro Algarvio <pedro@algarvio.me>
2020-05-13 11:21:09 +0100

    Add `random_string` utility

---
+++ tests/unit/utils/test_random_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.unit.utils.test_random_string
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Unit tests for the random string generator
+"""
+import pytest
+
+from saltfactories.utils import random_string
+
+
+def test_raises_runtimeerror_on_bad_arguments():
+    with pytest.raises(RuntimeError):
+        random_string("foo", uppercase=False, lowercase=False, digits=False)
