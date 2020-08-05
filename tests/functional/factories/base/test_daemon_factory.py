2e4fe76b544cd042bf018c4354611cb98b8937b1
Pedro Algarvio <pedro@algarvio.me>
2020-08-05 13:13:58 +0100

    Add some `pragma: no cover` comments

--- tests/functional/factories/base/test_daemon_factory.py
+++ tests/functional/factories/base/test_daemon_factory.py
@@ -14,7 +14,7 @@ from saltfactories.utils import platform
 PROCESS_START_TIMEOUT = 2
 
 
-def kill_children(procs):
+def kill_children(procs):  # pragma: no cover
     _, alive = psutil.wait_procs(procs, timeout=3)
     for p in alive:
         p.kill()
@@ -101,7 +101,7 @@ def test_daemon_process_termination(request, tempfiles):
     )
     daemon.terminate()
     assert psutil.pid_exists(daemon_pid) is False
-    for child in list(children):
+    for child in list(children):  # pragma: no cover
         if psutil.pid_exists(child.pid):
             continue
         children.remove(child)
@@ -253,7 +253,7 @@ def test_context_manager(request, tempfiles, start_timeout):
         started = None
         with daemon.started(start_timeout=start_timeout):
             # We should not even be able to set the following variable
-            started = False
+            started = False  # pragma: no cover
     assert started is None
     match = re.search(r".*\((?P<seconds>.*) seconds each\)", str(exc.value))
     assert match
@@ -302,7 +302,7 @@ def test_context_manager_returns_class_instance(tempfiles):
     with pytest.raises(RuntimeError):
         with daemon as d:
             # We should not even be able to set the following variable
-            started = d.is_running()
+            started = d.is_running()  # pragma: no cover
     assert d is None
     assert started is None
 
