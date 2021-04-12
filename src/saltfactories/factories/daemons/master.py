794b4fdef897733b49ac7ca023f3d2afe239a253
Pedro Algarvio <pedro@algarvio.me>
2021-04-12 18:35:38 +0100

    Salt allows minions and proxy minions to also have file and pillar roots configured

--- src/saltfactories/factories/daemons/master.py
+++ src/saltfactories/factories/daemons/master.py
@@ -53,12 +53,12 @@ class SaltMasterFactory(SaltDaemonFactory):
     @state_tree.default
     def __setup_state_tree(self):
         if "file_roots" in self.config:
-            return SaltStateTree(envs=copy.deepcopy(self.config["file_roots"]))
+            return SaltStateTree(envs=copy.deepcopy(self.config.get("file_roots") or {}))
 
     @pillar_tree.default
     def __setup_pillar_tree(self):
         if "pillar_roots" in self.config:
-            return SaltPillarTree(envs=copy.deepcopy(self.config["pillar_roots"]))
+            return SaltPillarTree(envs=copy.deepcopy(self.config.get("pillar_roots") or {}))
 
     @classmethod
     def default_config(
