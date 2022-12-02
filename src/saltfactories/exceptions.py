ab2baa2651a395a01f796ecf501a4ad463aad12a
Pedro Algarvio <palgarvio@vmware.com>
2022-12-02 18:52:53 +0000

    Drop support for Python 3.5 and 3.6
    Fixes #123
    
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- src/saltfactories/exceptions.py
+++ src/saltfactories/exceptions.py
@@ -1,35 +1,19 @@
 """
 PyTest Salt Factories related exceptions.
 """
-import sys
-
-if sys.version_info >= (3, 7):
-
-    def __getattr__(name):
-        if name in ("FactoryTimeout", "FactoryNotStarted"):
-            import pytestshellutils.exceptions  # pylint: disable=import-outside-toplevel
-            from saltfactories.utils import warn_until  # pylint: disable=import-outside-toplevel
-
-            warn_until(
-                "3.0.0",
-                "The '{}' exception is now in 'pytestshellutils.exceptions' and importing it "
-                "from 'saltfactories.exceptions' is deprecated and will cease to work after "
-                "pytest-salt-factories {{version}}.".format(name),
-            )
-            return getattr(pytestshellutils.exceptions, name)
-        else:
-            raise AttributeError("module '{}' has no '{}' attribute".format(__name__, name))
-
-else:
-    from pytestshellutils.exceptions import (  # noqa: F401  pylint: disable=unused-import
-        FactoryNotStarted,
-    )
-    from pytestshellutils.exceptions import (  # noqa: F401  pylint: disable=unused-import
-        FactoryTimeout,
-    )
 
 
-class SaltFactoriesException(Exception):
-    """
-    Base exception for all pytest salt factories.
-    """
+def __getattr__(name):
+    if name in ("FactoryTimeout", "FactoryNotStarted"):
+        import pytestshellutils.exceptions  # pylint: disable=import-outside-toplevel
+        from saltfactories.utils import warn_until  # pylint: disable=import-outside-toplevel
+
+        warn_until(
+            "3.0.0",
+            "The '{}' exception is now in 'pytestshellutils.exceptions' and importing it "
+            "from 'saltfactories.exceptions' is deprecated and will cease to work after "
+            "pytest-salt-factories {{version}}.".format(name),
+        )
+        return getattr(pytestshellutils.exceptions, name)
+    else:
+        raise AttributeError("module '{}' has no '{}' attribute".format(__name__, name))
